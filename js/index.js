a06db6fc83bc596b7564ed0f7bf3d0b08a6e3c93
SaulNeri12 <117798776+SaulNeri12@users.noreply.github.com>
2022-11-11 00:52:47 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,47 +1,80 @@
-
-
 const $softCard = document.querySelector(".soft-card").cloneNode(true);
 
 axios.get("https://raw.githubusercontent.com/SaulNeri12/sadm/main/methods.json")
   .then(response => {
 	const methods = response.data;
 
-	console.info(methods)
-
 	methods.forEach(m => {
-	  console.log(m.name);
-
 	  const $div = document.createElement("div");
-	  
-	  $div.classList.add("info-pane");
-	  $div.id = m.name;
+	  $div.classList.add("methods-info-pane");
 
 	  $div.innerHTML += `<h1 class="method-name">${m.name}</h1>`;
 	  $div.innerHTML += `<p class="method-desc">${m.description}</p>`;
-	  $div.innerHTML += `<img src="${m.picture}"/>`;
+	  $div.innerHTML += `
+		<div class="method-main-picture"> 
+		  <img src="${m.picture}"/>
+		</div>
+	  `;
 
-	  /*
-		TODO: NO SE PUEDE ITERAR JSON ARRAYS CON FOREACH(),
-		RESOLVER ESTO 
-		*/
+	  const $phasesSection = document.createElement("section");
+	  $phasesSection.classList.add("method-phases");
+	  $phasesSection.innerHTML = "<h1>Fases</h1>";
+
 	  m.phases.forEach(phase => {
-		$div.innerHTML += `<h2>${phase.name}</h2>`;
-		$div.innerHTML += `<p>${phase.description}</p>`;
-		phase.paragraphs.forEach(paragraph => {
-		  $div.innerHTML += `<p>${paragraph.text}</p>`;
+		const $phaseContainer = document.createElement("div");
+		const $phaseInfo = document.createElement("div");
+
+		$phaseContainer.classList.add("method-phase-container");
+		$phaseInfo.classList.add("method-phase-info");
+
+		$phaseContainer.innerHTML += `<h3 class="phase-method-name">+ ${phase.name}</h3>`;
+		// cuando el usuario haga click en el titulo de la fase, se desglosara la informacion...
+		$phaseContainer.querySelector(".phase-method-name").addEventListener("click", e => {
+		  //console.log(e.target);
+		  console.log(e.target);
+		  $phaseInfo.classList.toggle("hide");
+		});
+
+		// escribe la descripcion de cada fase de la metodologia
+		phase.description.paragraphs.forEach(paragraph => {
+		  $phaseInfo.innerHTML += `<p>${paragraph.text}</p>`;
+
+		  $phaseInfo.classList.add("hide");
+
+		  if (paragraph.resource) {
+			const $videoContainer = document.createElement("div");
+			$videoContainer.innerHTML += `<h4>${paragraph.resource.title}</h4>`;
+			//console.log(paragraph.resource.link);
+			const $iframe = document.createElement("iframe");
+			$iframe.classList.add("video-resource"); 
+			$iframe.src = paragraph.resource.link;
+
+			$videoContainer.appendChild($iframe);
+			$phaseInfo.appendChild($videoContainer);
+		  }
+
+		  // escribe las preguntas de cada parrafo, si es que las tiene...
 		  if (paragraph.questions.length > 0) {
-			const $ul = document.createElement("ul");
+			const $questionsList = document.createElement("ul");
 			paragraph.questions.forEach(question => {
 			  const $li = document.createElement("li");
 			  $li.textContent = question;
+			  $questionsList.appendChild($li);
 			});
+
+			$phaseInfo.appendChild($questionsList);
 		  }
-		})
+
+		  $phaseContainer.appendChild($phaseInfo);
+		  $phasesSection.appendChild($phaseContainer);
+		});
+
+		$div.appendChild($phasesSection);
 	  });
 
+	  // agrega la carta con la informacion del metodo a la etiqueta <main>
 	  document.body.querySelector("main").appendChild($div);
+	});
+  }).catch(error => {
+	  console.log(error);
 	})
-  })
-  .catch(error => {
-	console.log(error);
-  })
