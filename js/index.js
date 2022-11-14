be6b59ad5fb16510b616af2525bde96cf1b8e49d
SaulNeri12 <117798776+SaulNeri12@users.noreply.github.com>
2022-11-14 01:28:43 -0600

    Tabla de contenidos
    Anadi la tabla de contenidos y ahora trabaja correctamente

--- js/index.js
+++ js/index.js
@@ -1,80 +1,14 @@
-const $softCard = document.querySelector(".soft-card").cloneNode(true);
+import { loadMethodsInfo } from "./content.loader.js";
 
-axios.get("https://raw.githubusercontent.com/SaulNeri12/sadm/main/methods.json")
-  .then(response => {
-	const methods = response.data;
+const $ = id => document.querySelector(id);
 
-	methods.forEach(m => {
-	  const $div = document.createElement("div");
-	  $div.classList.add("methods-info-pane");
+loadMethodsInfo();
 
-	  $div.innerHTML += `<h1 class="method-name">${m.name}</h1>`;
-	  $div.innerHTML += `<p class="method-desc">${m.description}</p>`;
-	  $div.innerHTML += `
-		<div class="method-main-picture"> 
-		  <img src="${m.picture}"/>
-		</div>
-	  `;
+$("#menu-btn").onclick = function() {
+  $(".content-menu").classList.toggle("is-active");
+};
 
-	  const $phasesSection = document.createElement("section");
-	  $phasesSection.classList.add("method-phases");
-	  $phasesSection.innerHTML = "<h1>Fases</h1>";
+$(".content-menu").onclick = function(e) {
+  this.classList.toggle("is-active");
+}
 
-	  m.phases.forEach(phase => {
-		const $phaseContainer = document.createElement("div");
-		const $phaseInfo = document.createElement("div");
-
-		$phaseContainer.classList.add("method-phase-container");
-		$phaseInfo.classList.add("method-phase-info");
-
-		$phaseContainer.innerHTML += `<h3 class="phase-method-name">+ ${phase.name}</h3>`;
-		// cuando el usuario haga click en el titulo de la fase, se desglosara la informacion...
-		$phaseContainer.querySelector(".phase-method-name").addEventListener("click", e => {
-		  //console.log(e.target);
-		  console.log(e.target);
-		  $phaseInfo.classList.toggle("hide");
-		});
-
-		// escribe la descripcion de cada fase de la metodologia
-		phase.description.paragraphs.forEach(paragraph => {
-		  $phaseInfo.innerHTML += `<p>${paragraph.text}</p>`;
-
-		  $phaseInfo.classList.add("hide");
-
-		  if (paragraph.resource) {
-			const $videoContainer = document.createElement("div");
-			$videoContainer.innerHTML += `<h4>${paragraph.resource.title}</h4>`;
-			//console.log(paragraph.resource.link);
-			const $iframe = document.createElement("iframe");
-			$iframe.classList.add("video-resource"); 
-			$iframe.src = paragraph.resource.link;
-
-			$videoContainer.appendChild($iframe);
-			$phaseInfo.appendChild($videoContainer);
-		  }
-
-		  // escribe las preguntas de cada parrafo, si es que las tiene...
-		  if (paragraph.questions.length > 0) {
-			const $questionsList = document.createElement("ul");
-			paragraph.questions.forEach(question => {
-			  const $li = document.createElement("li");
-			  $li.textContent = question;
-			  $questionsList.appendChild($li);
-			});
-
-			$phaseInfo.appendChild($questionsList);
-		  }
-
-		  $phaseContainer.appendChild($phaseInfo);
-		  $phasesSection.appendChild($phaseContainer);
-		});
-
-		$div.appendChild($phasesSection);
-	  });
-
-	  // agrega la carta con la informacion del metodo a la etiqueta <main>
-	  document.body.querySelector("main").appendChild($div);
-	});
-  }).catch(error => {
-	  console.log(error);
-	})
